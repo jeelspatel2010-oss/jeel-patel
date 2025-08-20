c418271a3ddbb5c9931133c676c9f70ca6a55952
jeel patel <jeelspatel2010@gmail.com>
2025-08-20 23:55:01 +0530

    Add files via upload

---
+++ jeel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+      int a,b;
+
+      printf("Enter value of a and b\n");
+      scanf("%d%d", &a, &b);
+
+      printf("Addition of %d and %d is %d", a, b, (a+b));
+
+      return 0;
+ }
