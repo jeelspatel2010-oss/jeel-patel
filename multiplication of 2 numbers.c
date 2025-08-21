5feaeb26a183ca004a4d693dc23dfb8440b24b34
jeel patel <jeelspatel2010@gmail.com>
2025-08-21 21:54:35 +0530

    Add files via upload

---
+++ multiplication of 2 numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+  int main()
+  {
+  int a, b;
+
+  printf("Enter 2 numbers for multiplication\n");
+  scanf("%d %d",&a, &b);
+
+  printf("multiplication of %d and %d is %d\n", a, b, (a*b));
+
+  return 0;
+  }
