6acc59b5f5cc7fe6a4895aa9e5d6fde031140a29
jeel patel <jeelspatel2010@gmail.com>
2025-08-21 23:34:10 +0530

    Add files via upload

---
+++ divide 2 numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+  int main()
+ {
+  int a, b;
+
+  printf("Enter value for a and b,for division (a/b)");
+  scanf("%d%d", &a, &b);
+
+  printf("division of %d and %d is %d\n", a, b,(a/b));
+
+  return 0;
+ }
