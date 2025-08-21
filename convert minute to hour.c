ed2b9d9c3305601b9d78043c777fcc28059146c8
jeel patel <jeelspatel2010@gmail.com>
2025-08-21 23:36:02 +0530

    Add files via upload

---
+++ convert minute to hour.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int a,ans;
+   printf("Enter the value of minute=");
+   scanf("%d",&a);
+   ans=a/60;
+   printf("hour=%d",ans);
+   getch ();
+   }
