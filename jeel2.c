612a79cbcfb4f33286430a10fa2ae7973beb2155
jeel patel <jeelspatel2010@gmail.com>
2025-08-21 09:34:49 +0530

    Add files via upload
    subtraction of two numbers by C language.

---
+++ jeel2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+       int a, b;
+
+       printf("Enter value for a and b\n");
+       scanf("%d %d",&a, &b);
+
+       printf("subtraction of %d and %d is %d\n", a, b, (a-b));
+
+       return 0;
+ }
