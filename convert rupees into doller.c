ec74c06275f15b0b061921751a52ef072b0bd8d1
jeel patel <jeelspatel2010@gmail.com>
2025-08-21 23:36:57 +0530

    Add files via upload

---
+++ convert rupees into doller.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+     float inr,doller;
+
+     printf("Enter the amount in rupees:-");
+     scanf("%f",&inr);
+
+     doller=inr/48;
+     printf("Amount in DOLLER is:=%f",doller);
+     return 0;
+     }
